e3d859e4602fc74b2a0b22cef78997c305c1254e
Emanuel <emalencina16@gmail.com>
2026-06-08 21:31:07 -0300

    confing nginx y pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'gestion-backend',
+      cwd: './backend',
+      script: 'dist/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000,
+        DB_HOST: 'localhost',
+        DB_PORT: 5432,
+        DB_USERNAME: 'daw',
+        DB_PASSWORD: '2026',
+        DB_NAME: 'gestor_de_proyectos',
+        DB_LOGGING: false,
+        SWAGGER_HABILITADO: false,
+        JWT_SECRET: 'gtT0zY6&5Sx%7c29x&O4@^@73D&uz^xQ',
+      },
+      time: true,
+    },
+  ],
+};
